f7c036541a68adba779f756a7f058b914af3d586
Benjamin <134766715+Wilki406@users.noreply.github.com>
2023-09-08 11:54:51 +1000

    Create Toothbrush1.2.ino
    added the speaker feature, which plays when starting brushing sequence and when it automatically finishes via brush timer.

---
+++ Toothbrush1.2.ino
@@ -0,0 +1,150 @@
+// import libraries
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+
+// define u8g2 display
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0); 
+
+// constants (can change when running code)
+volatile byte state = 0;
+unsigned long previousMillis = 0UL;
+unsigned long interval = 120000UL;
+
+
+// Speaker pin
+int speakerPin = 8;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, INPUT_PULLUP);                  // define data pin for button
+  pinMode(6, INPUT_PULLUP);                  // define data pin for button
+  u8g2.begin();                              // initialise u8g2
+  Serial.begin(9600);                        // initialise serial monitor
+  Serial.println("Reset");                   // print "reset" in serial monitor
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (state == 0){
+    u8g2.clearBuffer();                      // clear the internal memory
+    u8g2.setFont(u8g2_font_logisoso28_tr);   // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+    u8g2.drawStr(20,29,"Brush?");            // write something to the internal memory
+    u8g2.sendBuffer();                       // transfer internal memory to the display
+
+    if (digitalRead(4) == LOW){
+      delay(200);
+      state = 5;
+      Serial.println("state1");
+    }
+
+    if (digitalRead(6) == LOW){
+      delay(200);
+      state = 2;
+      Serial.println("state2");
+    }
+  }
+
+  if (state == 1){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_logisoso28_tr);
+    u8g2.drawStr(0,29,"brushing");           // write something to the internal memory
+    u8g2.sendBuffer();                       // transfer internal memory to the display
+
+    if (digitalRead(4) == LOW)
+    {
+      delay(200);
+      state = 0;
+      Serial.println("state0");
+    }
+    unsigned long currentMillis = millis();
+
+    if(currentMillis - previousMillis > interval)
+    {
+      delay(500);
+      state = 6;
+      Serial.println("state4timed");
+      // update the previousMillis value
+      previousMillis = currentMillis;
+    }
+
+  }
+
+  if (state == 2){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_logisoso28_tr);
+    u8g2.drawStr(0,29,"100%");           // write something to the internal memory
+    u8g2.sendBuffer();                       // transfer internal memory to the display
+
+    if (digitalRead(4) == LOW)
+    {
+      delay(200);
+      state = 1;
+      Serial.println("state1");
+    }
+    
+    if(digitalRead(6) == LOW)
+    {
+      delay(200);
+      state = 3;
+      Serial.println("state3");
+
+    }
+  }
+
+  if (state == 3){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_logisoso28_tr);
+    u8g2.drawStr(0,29,"2 MIN");           // write something to the internal memory
+    u8g2.sendBuffer();                       // transfer internal memory to the display
+
+    if (digitalRead(4) == LOW)
+    {
+      delay(200);
+      state = 1;
+      Serial.println("state1");
+    }
+    
+    if(digitalRead(6) == LOW)
+    {
+      delay(200);
+      state = 2;
+      Serial.println("state2");
+    }
+  }
+
+  if (state == 4){ u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_logisoso28_tr);
+    u8g2.drawStr(0,29,"Thanks");           // write something to the internal memory
+    u8g2.sendBuffer();                       // transfer internal memory to the display
+
+    delay(1500);
+    state = 0;
+  }
+
+  if (state == 5){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_logisoso28_tr);
+    u8g2.drawStr(0,29,"brushing");           // write something to the internal memory
+    u8g2.sendBuffer();
+
+    tone(speakerPin, 1000, 150);
+
+    delay(150);
+    state = 1;
+  
+  }
+
+  if (state == 6){
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_logisoso28_tr);
+    u8g2.drawStr(0,29,"Thanks");           // write something to the internal memory
+    u8g2.sendBuffer();
+
+    tone(speakerPin, 1000, 150);
+
+    delay(150);
+    state = 4;
+  
+  }
+}
