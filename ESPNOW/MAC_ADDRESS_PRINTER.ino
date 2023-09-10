000b301103e09e2b00198bb7cc0ce00203e882eb
Benjamin <134766715+Wilki406@users.noreply.github.com>
2023-09-10 17:57:40 +1000

    Create MAC_ADDRESS_PRINTER.ino

---
+++ ESPNOW/MAC_ADDRESS_PRINTER.ino
@@ -0,0 +1,17 @@
+#include "WiFi.h"
+ 
+void setup() {
+ 
+  // Setup Serial Monitor
+  Serial.begin(115200);
+ 
+  // Put ESP32 into Station mode
+  WiFi.mode(WIFI_MODE_STA);
+ 
+  // Print MAC Address to Serial monitor
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
+ 
+void loop() {
+}
